dc03cbc78c18bd17af37b543e536ec6659049beb
Julie Deng <shuyindeng@berkeley.edu>
2017-08-31 16:11:23 +0000

    hw0

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,45 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.reduce(0, :+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  arr.empty? ? 0 : arr.sort.last(2).reduce(:+)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.length < 2 ? false : (arr.combination(2).find{|x,y| x + y == n } ? true : false)
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+   /^[bcdfghjklmnpqrstvwxyz]/i === s
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  s =~ /^[01]*1[01]*00$/ || s == "0"
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+
+  attr_accessor :isbn, :price
+
+  def initialize isbn, price
+    @isbn = isbn
+    @price = price
+    raise ArgumentError if(@isbn===""||@price<=0)
+  end
+
+  def price_as_string
+    format("$%.2f", @price)
+  end
+
 end
